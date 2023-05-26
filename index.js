022bc1d29ffcf9796fafa8b5a54419600a19d461
Diagonal Dev <diagonaldev23@gmail.com>
2023-05-26 12:18:19 +0200

    conexion front back y bdd

--- index.js
+++ index.js
@@ -1,74 +1,97 @@
-require('dotenv').config()
+require('dotenv').config();
 const fs = require('fs');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// mongoose
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Conexion a la base de datos realizada correctamente!');
+  })
+  .catch((err) => {
+    console.log('ERROR al conectarse a la base de datos.', err);
+  });
+
+const taskSchema = new Schema({
+  name: String,
+  done: Boolean,
+});
+
+const Task = mongoose.model('Task', taskSchema);
 
 // Devolver archivos tipo json()
-app.use(express.json())
+app.use(express.json());
 
 // Servir archivos estaticos
 app.use(express.static('public'));
 
 // probando un middleware
 app.use((req, res, next) => {
-    console.log("Middleware 1")
-    next();
-})
+  console.log('Middleware 1');
+  next();
+});
 
 // Configurar rutas
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/users', (req, res) => {
-  res.json([{name:'Juan', edad: 20}, {name:'Maria', edad: 22}])
-})
+  res.send('Hello World!');
+});
 
 app.get('/api/tasks', (req, res) => {
-    const data = require('./tareas.json')
-    console.log(data. tareas)
-    res.json(data.tareas)
-  })
+  Task.find()
+    .then((tasks) => {
+      res.status(200).json({ ok: true, data: tasks });
+    })
+    .catch((err) => {
+      res.status(400).json({ ok: false, message: 'Hubo un error al obtener las tareas' });
+    });
+});
 
-app.post("/api/tasks", (req, res) => {
-    const body = req.body
-    console.log(body)
-    fs.readFile('./tareas.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error al leer el archivo:', err);
-          return;
-        }
-      
-        // Parsear el contenido JSON en un objeto JavaScript
-        const tareas = JSON.parse(data);
-      
-        // Crear la nueva tarea
-        const nuevaTarea = {
-          id: 5,
-          titulo: body.text,
-          descripcion: 'Realizar ejercicio físico durante 30 minutos',
-          estado: 'pendiente',
-          fechaCreacion: '2023-05-22',
-          fechaLimite: '2023-05-25'
-        };
-      
-        // Agregar la nueva tarea al arreglo de tareas existentes
-        tareas.tareas.push(nuevaTarea);
-      
-        // Escribir el contenido actualizado en el archivo
-        fs.writeFile('./tareas.json', JSON.stringify(tareas), 'utf8', (err) => {
-          if (err) {
-            console.error('Error al agregar la tarea:', err);
-            return;
-          }
-          console.log('Nueva tarea agregada correctamente.');
-        });
-      });
-    res.status(201).json({ok: true, message: 'Tarea creada con éxito'})
-})
+app.post('/api/tasks', (req, res) => {
+  const body = req.body;
+  console.log(body);
+  const task1 = Task.create({
+    name: body.text,
+    done: false,
+  })
+    .then((createdTask) => {
+      res.status(201).json({ ok: true, message: 'Tarea creada con éxito', data: createdTask });
+    })
+    .catch((err) => {
+      res.status(400).json({ ok: false, message: 'Error al crear la tarea' });
+    });
+});
+
+app.put('/api/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  console.log(body);
+  const task1 = Task.findByIdAndUpdate(id, {
+    name: body.text,
+  })
+    .then((updatedTask) => {
+      res.status(200).json({ ok: true, message: 'Tarea editada con éxito', data: updatedTask });
+    })
+    .catch((err) => {
+      res.status(400).json({ ok: false, message: 'Error al editar la tarea' });
+    });
+});
+
+app.delete('/api/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  Task.findByIdAndRemove(id)
+    .then((deletedTask) => {
+      res.status(200).json({ ok: true, data: deletedTask });
+    })
+    .catch((err) => {
+      res.status(400).json({ ok: false, message: 'Hubo un error al eliminar la tarea' });
+    });
+});
 
 // Poner a escuchar la APP en un puerto
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
--- public/index.js
+++ public/index.js
@@ -1,48 +1,90 @@
-window.addEventListener('load', function() {
-    console.log('La página ha terminado de cargarse!!');
+window.addEventListener('load', function () {
+  console.log('La página ha terminado de cargarse!!');
 });
 
-const getBtn = document.querySelector('#get-tasks')
-const createBtn = document.querySelector('#create-task')
-const input = document.querySelector('#task-name')
+const createEditBtn = document.querySelector('#create-task');
+const input = document.querySelector('#task-name');
+const tasksContainer = document.querySelector('#tasks-container');
 
-getBtn.addEventListener('click', async () => {
-    console.log('GET TAREAS')
-    let data = ''
-    try {
-        const response = await fetch('https://emprende-back-nqtq.onrender.com/api/tasks')
-        if(!response.ok) {
-            throw new Error('Error al obtener datos')
-        }
-        data = await response.json();
-        console.log(data)
-        
-    } catch (error) {
-        console.error(error)
-    }
+let TASK_TO_EDIT = null;
 
-    data.forEach(element => {
-        
-        const respuesta = document.createElement('div')
-        respuesta.innerHTML = `
-        <h2>${element.titulo}</h2>
-        <p>${element.descripcion}</p>
-        <p>Estado: ${element.estado}</p>
-        <p>Fecha de creación: ${element.fechaCreacion}</p>
-        <p>Fecha límite: ${element.fechaLimite}</p>
-      `;
-        document.querySelector('body').appendChild(respuesta)
+console.log(window);
+const baseURL = '/api';
+
+createEditBtn.addEventListener('click', async () => {
+  const creating = !TASK_TO_EDIT;
+  try {
+    const path = creating ? 'tasks' : `tasks/${TASK_TO_EDIT._id}`;
+    const methodToDo = creating ? 'POST' : 'PUT';
+    const res = await fetch(`${baseURL}/${path}`, {
+      method: methodToDo,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: input.value }),
     });
-    
-})
+    if (!res.ok) {
+      throw new Error('Error al crear tarea');
+    }
+    const data = await res.json();
+    input.value = '';
+    createEditBtn.innerText = 'Crear tarea';
+    getTasks();
+    console.log(data);
+  } catch (error) {}
+});
 
-createBtn.addEventListener('click', async () => {
-    console.log('CREATE TAREAS')
-    console.log({input})
-    fetch('https://emprende-back-nqtq.onrender.com/api/tasks', {
-        method: 'POST',
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({text: input.value})
-    })
+async function getTasks() {
+  tasksContainer.innerHTML = '';
+  try {
+    const response = await fetch(`${baseURL}/tasks`);
+    if (!response.ok) {
+      throw new Error('Error al obtener datos');
+    }
+    const { data } = await response.json();
+    data.forEach((element) => {
+      const taskContainerDiv = document.createElement('div');
+      const taskParagraph = document.createElement('p');
+      const deleteTaskBtn = document.createElement('button');
 
-})
+      taskContainerDiv.classList.add('container-div');
+
+      taskParagraph.innerText = element.name;
+      taskParagraph.addEventListener('click', () => {
+        input.value = element.name;
+        createEditBtn.innerText = 'Editar tarea';
+        TASK_TO_EDIT = element;
+      });
+
+      deleteTaskBtn.innerText = 'Borrar Tarea';
+      deleteTaskBtn.setAttribute('id', element._id);
+      deleteTaskBtn.classList.add('button', 'button-red');
+      deleteTaskBtn.addEventListener('click', (event) => {
+        const taskId = event.target.id;
+        deleteTaskBtn.innerText = 'Borrando...';
+        fetch(`${baseURL}/tasks/${taskId}`, {
+          method: 'DELETE',
+        }).then(() => {
+          const taskDiv = deleteTaskBtn.parentElement;
+          taskDiv.remove();
+        });
+      });
+
+      taskContainerDiv.appendChild(taskParagraph);
+      taskContainerDiv.appendChild(deleteTaskBtn);
+
+      tasksContainer.appendChild(taskContainerDiv);
+    });
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+input.addEventListener('input', (event) => {
+  console.log('Evento del input', event);
+  if (event.target.value.trim() === '') {
+    createEditBtn.innerText = 'Crear tarea';
+    TASK_TO_EDIT = null;
+  }
+});
+
+getTasks();
